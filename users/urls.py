ea4b3b880070be6aa60d4f818bd74df5bf30667f
tagarwal698 <77000381+tagarwal698@users.noreply.github.com>
2021-08-19 20:47:05 +0530

    Create urls.py

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .import views 
+
+app_name = "users"
+
+urlpatterns = [
+	
+	path('', views.AccountView.as_view(), name = "account"),
+	path('profile', views.ProfileView, name = "profile"),
+	path('sign-up', views.SignUpView.as_view(), name = "sign-up"),
+	path('sign-in', views.SignInView.as_view(), name = "sign-in"),
+	path('sign-out', views.sign_out, name= "sign-out"),
+
+
+]
